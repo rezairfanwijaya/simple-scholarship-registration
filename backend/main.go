57c397950d8e183e19284d7f50735fc3a32f8b99
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-25 15:45:54 +0700

    add: endpoint get count status approval

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"serkom/admin"
 	"serkom/approval"
+	"serkom/auth"
 	"serkom/database"
 	"serkom/handler"
 	"serkom/helper"
 	"serkom/mahasiswa"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
@@ -23,6 +28,9 @@ func main() {
 	// allow cors
 	router.Use(CorsMiddleware())
 
+	// auth
+	authService := auth.NewAuthService()
+
 	// approval
 	repoApproval := approval.NewRepository(conn)
 	serviceApproval := approval.NewService(repoApproval)
@@ -32,10 +40,18 @@ func main() {
 	serviceMahasiswa := mahasiswa.NewService(repoMahasiswa, serviceApproval)
 	hanlderMahasiswa := handler.NewHandlerMahasiswa(serviceMahasiswa)
 
+	// admin
+	// admin
+	repoAdmin := admin.NewRepository(conn)
+	serviceAdmin := admin.NewService(repoAdmin, serviceApproval)
+	handlerAdmin := handler.NewHandlerAdmin(serviceAdmin, authService)
+
 	// endpoint
 	router.GET("/mahasiswa/:email", hanlderMahasiswa.GetByEmail)
 	router.POST("/mahasiswa/daftar", hanlderMahasiswa.DaftarBeasiswa)
 	router.GET("/mahasiswa/approval", hanlderMahasiswa.GetAllApprovals)
+	router.POST("/admin/login", handlerAdmin.Login)
+	router.GET("/admin/status/show", handlerAdmin.GetTotalStatus)
 	router.Static("/image", "./images")
 
 	env, err := helper.GetENV(".env")
@@ -46,6 +62,79 @@ func main() {
 	// start server
 	if err := router.Run(env["DOMAIN"]); err != nil {
 		log.Fatal(err.Error())
+	}
+}
+
+// function middleware auth
+func authMiddleware(authService auth.Service, adminService admin.Service, typeUser string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ambil nilai header yang sudah kita set namanya Authorization
+		authHeader := c.GetHeader("Authorization")
+
+		// cek apakah nilai authorization memiliki Bearer
+		// karena nanti kita akan set nilai token seperti ini "Bearer djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"
+		if !strings.Contains(authHeader, "Bearer") {
+			respons := helper.GenerateResponse(
+				"gagal",
+				http.StatusUnauthorized,
+				"Unauthorized",
+			)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// lalu kita pisahkan tokennya berdasarkan spasi
+		// before "Bearer djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"
+		// after ["Bearer"] ["djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"]
+		tokenString := ""
+		arraytoken := strings.Split(authHeader, " ")
+		if len(arraytoken) == 2 {
+			tokenString = arraytoken[1]
+		}
+
+		// validasi token
+		token, err := authService.VerifyToken(tokenString)
+		if err != nil {
+			respons := helper.GenerateResponse(
+				"gagal",
+				http.StatusUnauthorized,
+				"Unauthorized",
+			)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// ambil data dalam token
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			respons := helper.GenerateResponse(
+				"gagal",
+				http.StatusUnauthorized,
+				"Unauthorized",
+			)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// data id user diambil
+		userId := int(claim["user_id"].(float64))
+
+		if typeUser == "admin" {
+			// data user diambil berdasarkan id
+			user, err := adminService.GetUserByID(userId)
+			if err != nil {
+				respons := helper.GenerateResponse(
+					"gagal",
+					http.StatusUnauthorized,
+					"Unauthorized",
+				)
+				c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+				return
+			}
+
+			// set context
+			c.Set("currentAdmin", user)
+		}
 	}
 }
 
