75fee2a5e7daf0b6bd4e50117d1a3423576f0965
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-25 15:43:45 +0700

    add: dashboard and login pages

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "../components/SideBar";
+
+const Dashboard = () => {
+    return (<>
+        <div className="dashboard">
+            <div className="main md:flex justify-between gap-11">
+                <section className="md:1/3 lg:w-1">
+                    <SideBar />
+                </section>
+
+                <section className="md:w-4/6 lg:w-10/12 lg:px-24">
+                    <main>
+                        <Outlet />
+                    </main>
+                </section>
+            </div>
+        </div>
+
+
+    </>);
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import FormLogin from "../components/FormLogin";
+
+const Login = () => {
+    return (<>
+        <div className="form lg:px-96 lg:py-48">
+            <FormLogin/>
+        </div>
+    </>);
+}
+
+export default Login;
