a6cf750600b074e782de20d4a98c0fd2292e6aaa
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-25 07:02:59 +0700

    add: endpoint get mahasiswa by email

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"serkom/database"
+	"serkom/handler"
+	"serkom/helper"
+	"serkom/mahasiswa"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -11,5 +16,44 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	log.Println(conn)
+	// init router
+	router := gin.Default()
+
+	// allow cors
+	router.Use(CorsMiddleware())
+
+	// mahasiswa
+	repoMahasiswa := mahasiswa.NewRepository(conn)
+	serviceMahasiswa := mahasiswa.NewService(repoMahasiswa)
+	hanlderMahasiswa := handler.NewHandlerMahasiswa(serviceMahasiswa)
+
+	// endpoint
+	router.GET("/mahasiswa/:email", hanlderMahasiswa.GetByEmail)
+
+	env, err := helper.GetENV(".env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// start server
+	if err := router.Run(env["DOMAIN"]); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+// CORS
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		c.Next()
+	}
 }
