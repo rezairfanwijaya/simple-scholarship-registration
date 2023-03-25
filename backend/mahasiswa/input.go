c9c9cc6792ba2252d253b6e3bb2ac956542619d3
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-25 08:16:23 +0700

    add: input for daftar beasiswa

--- backend/mahasiswa/input.go
+++ backend/mahasiswa/input.go
@@ -1,1 +1,11 @@
 package mahasiswa
+
+type InputDaftarBeasiswa struct {
+	Nama          string
+	Email         string
+	Telepon       string
+	Semester      int
+	IPK           float64
+	BerkasURL     string
+	JenisBeassiwa string
+}
