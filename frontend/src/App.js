ddc78713a73284a8e88147319b4461ce7fe009ed
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-25 15:42:56 +0700

    add: routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Daftar from './pages/Daftar';
 import Hasil from './pages/Hasil';
+import Login from './pages/Login';
+import PrivateRoute from './utils/auth';
+import Dashboard from './pages/Dashboard';
+import Index from './components/dashboard/Index';
 
 function App() {
   return (<>
@@ -13,20 +17,39 @@ function App() {
         <Route
           path={"/"}
           exact
-          element={<LandingPage/>}
+          element={<LandingPage />}
         />
 
         <Route
           path={"/daftar"}
           exact
-          element={<Daftar/>}
+          element={<Daftar />}
         />
 
         <Route
           path={"/hasil"}
           exact
-          element={<Hasil/>}
+          element={<Hasil />}
         />
+
+        <Route
+          path={"/login"}
+          exact
+          element={<Login />}
+        />
+
+        <Route
+          path={"/dashboard"}
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+
+          <Route path={""} element={<Index />} />
+          {/* <Route path={"alumni"} element={<Alumni />} /> */}
+        </Route>
 
       </Routes>
     </div>
